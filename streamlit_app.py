27f0b79e31d2b9227dcfae92286ca114798f32d1
cesarclave <135029299+cesarclave@users.noreply.github.com>
2024-03-11 15:06:17 +0100

    add nutrition details for all fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -24,6 +25,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen+'Nutrution Information')
+        fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -38,9 +42,4 @@ if ingredients_list:
        session.sql(my_insert_stmt).collect()
        st.success('Your Smoothie is ordered'+', '+name_on_order+'!', icon="✅")
 
-# New section to display fruityvice nutrition information
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.write("""Details"")
-st.text(fruityvice_response.json())
-#fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
