c3c6e67fafe88a9c3581805aef4ad670bfab0028
cesarclave <135029299+cesarclave@users.noreply.github.com>
2024-03-11 15:40:26 +0100

    fix typo again

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.write('The name on the smoothie: ', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_with=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:'
